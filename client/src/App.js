a0ef2b61459fabf6aafd18fc0369b498ed8304c3
Johan Bouwsema <dev@shoebil.nl>
2022-09-12 21:02:35 +0200

    remove jwt token

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     fetch("/users/me", {
       method: 'GET',
       headers: {
-        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzFmNmE5Njc5ZGY4MTliNDE2ODdmMzAiLCJpYXQiOjE2NjMwMDMyODZ9.8vIahLS6Uq6HWAINz3xMI28YylnC-jHRVAuzGhMrxK4`
+        'Authorization': `Bearer `
       }
     }).then(response => response.json()).then(data => setUserName(data.name))
   }, [])
